ff36edb047ee18fe20050237514489b03eff950f
dra6unov <dra6unov@gmail.com>
2017-10-27 21:24:10 +0500

    только

---
+++ Pipe Calc/Pipe Calc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pipe_Calc.MvcApplication" Language="C#" %>
---
+++ Pipe Calc/Pipe Calc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pipe_Calc.Startup))]
+namespace Pipe_Calc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
