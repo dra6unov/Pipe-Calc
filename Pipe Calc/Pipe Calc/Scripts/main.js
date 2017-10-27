cdb2e1495cccc075e52c3a7f6ba40f69687c9f06
MaxAvatar <maxavatar007@gmail.com>
2017-10-28 01:00:56 +0300

    fixed this fucking shit

--- Pipe Calc/Pipe Calc/Scripts/main.js
+++ Pipe Calc/Pipe Calc/Scripts/main.js
@@ -1,80 +1,70 @@
 
-$('#btn1').click(function (event) {
+$("#btn1").click(function(event) {
     event.preventDefault();
-   data= {
-        "Ymu" : $("#DDL1").val(),
-
-        "Yd" : $("#DDL2").val(),
-            
-        "Sigmau" : $("#value_1").val(),
+    var data = {
+        "Ymu": $("#DDL1").val(),
+        "Yd": $("#DDL2").val(),
+        "Sigmau": $("#value_1").val(),
         "Sigmay": $("#value_2").val(),
         "Yfp": $("#value_3").val(),
         "p": $("#value_4").val(),
         "D": $("#value_5").val()
-        };
-	
+    };
+
     $.ajax({
-        url: '/Compute',
-        type: 'POST',
+        url: "/Compute",
+        type: "POST",
         data: data,
-        success: function (result) {
-            $('.answer').append('<p>' + result.Message + '</p>')
+        success: function(result) {
+            $(".answer").append("<p>" + result.Message + "</p>");
         },
-        error: function (result) {
-            alert('Oh no :(');
+        error: function(result) {
+            alert("Oh no :(");
         }
     });
 });
 
+function get_shit() {
+    console.log($("#DDL1").val() + " " + $("#DDL2").val());
+    if ($("#DDL2").val() == "Низкое") {
+        switch ($("#DDL1").val()) {
+        case "H":
+            return "0,82";
+        case "C":
+            return "0,767";
+        case "B":
+            return "0,637";
+        }
+    } else {
+        switch ($("#DDL1").val()) {
+        case "H":
+            return "0,767";
+        case "C":
+            return "0,637";
+        case "B":
+            return "0,51";
+        }
+    }
+}
 
-$('#btn2').click(function (event) {
+$("#btn2").click(function(event) {
     event.preventDefault();
-    data = {
-        "Sigmay": $("#value_1").val(),
-        "Yfp": $("#value_2").val(),
-        "p": $("#value_3").val(),
-        "D": $("#value_4").val(),
-        "C": $("#value_5").val()
-    };
-    if ($("#DDL1").val() == "H") {
-        switch ($("#DDL2").val()) {
-            case "Низкое":
-                data["Yds"] = "0.82";
-                break;
-            case "Среднее":
-                data["Yds"] = "0.767";
-                break;
-        }
-    }
-    else if ($("#DDL1").val() == "C") {
-        switch ($("#DDL2").val()) {
-            case "Низкое":
-                data["Yds"] = "0.767";
-                break;
-            case "Среднее":
-                data["Yds"] = "0.637";
-                break;
-        }
-    }
-    else {
-        switch ($("#DDL2").val()) {
-            case "Низкое":
-                data["Yds"] = "0.637";
-                break;
-            case "Среднее":
-                data["Yds"] = "0.51";
-                break;
-        }
-    }
     $.ajax({
-        url: '/Another/Compute',
-        type: 'POST',
-        data: data,
-        success: function (result) {
-            $('.answer').append('<p>' + result.Message + '</p>')
+        url: "/Another/Compute",
+        type: "POST",
+        data: {
+            "Sigmay": $("#value_1").val(),
+            "Yfp": $("#value_2").val(),
+            "p": $("#value_3").val(),
+            "D": $("#value_4").val(),
+            "C": $("#value_5").val(),
+            "Yds": get_shit()
         },
-        error: function (result) {
-            alert('Oh no :(');
+        success: function(result) {
+            $(".answer").append("<p>" + result.Message + "</p>");
+        },
+        error: function(result) {
+            alert("Oh no :(");
         }
     });
 });
